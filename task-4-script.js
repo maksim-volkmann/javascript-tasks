e835a7f7ee9fbe5f6a51fcabb543b21c722c27f3
Maksim <auksinistonic@gmail.com>
2022-08-24 17:45:11 +0300

    Initial commit

---
+++ task-4-script.js
@@ -0,0 +1,9 @@
+// 2. Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
+// Author: Maksim Volkmann
+
+function myfunc(str) {
+  // console.log(str.slice(0, -3))
+  return str.slice(0, -3)
+}
+
+myfunc('Remove this')
---
+++ task-5-script.js
@@ -0,0 +1,9 @@
+// 5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
+// Author: Maksim Volkmann
+
+function takeTwoArrays(arr1, arr2) {
+  const mergeArr = arr1.concat(arr2)
+  return mergeArr
+}
+
+takeTwoArrays([1, 2, 3], ['a', 'r', 'd'])
