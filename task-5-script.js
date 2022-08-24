60d3380638d0a1033c4c979d64e65b0f800aa0c3
Maksim <auksinistonic@gmail.com>
2022-08-24 18:30:15 +0300

    update: update task 5

--- task-5-script.js
+++ task-5-script.js
@@ -3,8 +3,7 @@
 function takeTwoArrays(arr1, arr2) {
   const mergeArr = arr1.concat(arr2).sort()
   const noDubs = [...new Set(mergeArr)]
-  // console.log(noDubs)
   return noDubs
 }
 
-takeTwoArrays([3, 2, 1, 3, 3], ['a', 'r', 'd', 'a'])
+console.log(takeTwoArrays([3, 2, 9, 'c', 3, 3], ['a', 'r', 'd', 1, 'a']))
