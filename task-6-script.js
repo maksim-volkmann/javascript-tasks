d8824996c4bd69c91d066b0225b4ed31b246769d
Maksim <auksinistonic@gmail.com>
2022-08-24 18:23:39 +0300

    update: task 6 done

--- task-6-script.js
+++ task-6-script.js
@@ -1,1 +1,21 @@
 // Write a function that takes an object (a) and a string (b) as argument  Return true if the object has a property with key 'b' Return false otherwise
+
+const myObj = {
+  a: 'keyA',
+  // b: 'keyB',
+  c: 'keyC',
+}
+
+const myStr = 'b'
+
+function testMyProperty(myObj, myStr) {
+  if (myObj.hasOwnProperty(myStr)) {
+    // console.log('true')
+    return true
+  } else {
+    // console.log('false')
+    return false
+  }
+}
+
+testMyProperty(myObj, myStr)
